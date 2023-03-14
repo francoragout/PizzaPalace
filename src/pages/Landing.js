9b0f12fcc116a59e64b5cb40e38e1e3f6d32825f
francoragout <francoragout1@gmail.com>
2023-03-14 04:52:40 -0300

    modal was dealted

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,10 +2,14 @@ import React from 'react';
 import "./Landing.css";
 import Navbar from "../components/Navbar/Navbar";
 
+
 const Landing = () => {
   return (
     <>
     <Navbar />
+    
+    
+    
 
     </>
   )
